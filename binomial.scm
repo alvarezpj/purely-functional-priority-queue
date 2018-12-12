684fdb9b43d9d9980fa7059a2ebc662f8e83c4cc
Victor Alvarez Pajaro <vicalpa@protonmail.com>
2018-12-12 12:31:45 -0500

    Add binomial.scm

---
+++ binomial.scm
@@ -0,0 +1,8 @@
+; Binomial Queue
+
+; supported operations:
+;    * findMin(q)   - return minimum element of queue q
+;    * insert(e, q) - insert element e into queue q
+;    * meld(q1, q2) - merge queues q1 and q2 into a single queue
+;    * deleteMin(q) - remove minimum element of queue q
+
